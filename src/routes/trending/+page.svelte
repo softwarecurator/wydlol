2797b097438f6023f9d28275b0b597aa2318fdec
Joseph Rios <blankworldwide@gmail.com>
2022-08-29 14:53:03 -0500

    feat: fix dropdown

--- src/routes/trending/+page.svelte
+++ src/routes/trending/+page.svelte
@@ -22,8 +22,10 @@
 		/>
 	</div>
 	<select bind:value={contractAddress}>
-		<option selected value="0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D">Bored Ape Yacht Club</option
+		<option selected value={contractAddress}
+			>{trending.activities[0].collection.collectionName}</option
 		>
+		<option value="0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D">Bored Ape Yacht Club</option>
 		<option value="0x23581767a106ae21c074b2276D25e5C3e136a68b">Moonbirds</option>
 		<option value="0xbCe3781ae7Ca1a5e050Bd9C4c77369867eBc307e">goblintown.wtf</option>
 	</select> LOL
