6a07b262c4f395423cfffc77c3cb24437df73642
Joseph Rios <blankworldwide@gmail.com>
2022-08-28 23:34:04 -0500

    feat: server moralis

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,20 @@
 import { getCookies } from '$lib/utilities/getCookies';
 import Moralis from 'moralis/node';
+import { env } from '$env/dynamic/public';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ request }) {
     const cookies = getCookies(request);
     if (cookies['wyd-user'] && cookies['wyd-session']) {
         const lower = cookies['wyd-user'].toLowerCase();
+        Moralis.start({
+            serverUrl: env.PUBLIC_MORALIS_SERVER_URL,
+            appId: env.PUBLIC_MORALIS_APP_ID
+        });
         const user = await new Moralis.Query(Moralis.Object.extend('Profile'))
             .containedIn('accounts', [lower])
             .first();
+
         return {user};
     }
-
 }
